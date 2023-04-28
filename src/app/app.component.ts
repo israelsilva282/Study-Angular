e73a652f0588769479abd3e4b0d81b8c9c11972f
israelsilva282 <israelcostas@hotmail.com>
2023-04-28 10:12:05 -0300

    Consertado algumas configurações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'studyangular';
+  userData = {
+    email: 'user@email.com',
+    role: 'Admin',
+  };
+
+  userName = 'Joaquim';
+
+  title = 'teoria';
 }
--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
-  styleUrls: ['./parent-data.component.css']
+  styleUrls: ['./parent-data.component.css'],
 })
-export class ParentDataComponent {
-@Input() name: string ='';
+export class ParentDataComponent implements OnInit {
+  @Input() user!: { email: string; role: string };
+  @Input() name: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
