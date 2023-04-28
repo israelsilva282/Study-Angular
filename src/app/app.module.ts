73bbb4a712d3b8868372a9e3dc954eb25c73251c
israelsilva282 <israelcostas@hotmail.com>
2023-04-28 09:40:12 -0300

    Adicionado diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
